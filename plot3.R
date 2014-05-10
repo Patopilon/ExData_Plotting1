e46ccf66c120c6e8386841e4cb34a7dc674eebee
patricio diaz <patopilon@gmail.com>
2014-05-10 11:42:58 -0500

    final update

--- plot3.R
+++ plot3.R
@@ -34,16 +34,8 @@ legend("topright",
        lty=1,y.intersp=0.5,
        col=c("black","red","blue"),cex=0.8) 
 
-legend(data$dataTime[1850],50,lty=1,
-       c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
-       y.intersp=0.5,
-       col=c("black","red","blue"),cex=0.8) 
-
-legend(x="topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
-       col=c("black","red","blue"),lty=1) 
-
 ###########################
 # save as png
 
-dev.copy(png, file = "plot3.png",height=504, width=504, bg="transparent")
+dev.copy(png, file = "plot3.png",height=480, width=480, bg="transparent")
 dev.off()
